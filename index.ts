b78b3d4eba03584358763f88293feb4dd89762ce
axtk <axtkx@inbox.ru>
2026-03-15 22:07:11 +0300

    add getArgValue and getArgValues

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
+export * from "./src/getArgValue.ts";
+export * from "./src/getArgValues.ts";
 export * from "./src/isKey.ts";
 export * from "./src/parseArgs.ts";
---
+++ src/getArgValue.ts
@@ -0,0 +1,13 @@
+import { isKey } from "./isKey.ts";
+
+export function getArgValue(argName: string, fallback: string): string;
+export function getArgValue(argName: string): string | undefined;
+
+export function getArgValue(argName: string, fallback?: string) {
+  let { argv } = process;
+  let k = argv.indexOf(argName);
+
+  return k > 1 && argv[k + 1] && !isKey(argv[k + 1])
+    ? argv[k + 1]
+    : fallback;
+}
