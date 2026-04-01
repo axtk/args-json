e61488243ea081cb5c2c511c750d4f54e372a7b3
axtk <axtkx@inbox.ru>
2026-04-01 11:57:28 +0300

    lint

--- src/isKey.ts
+++ src/isKey.ts
@@ -1,6 +1,6 @@
 export function isKey(x: string) {
   return (
-    ((x.startsWith("-") && x.length === 2 && x !== "--") ||
-      (x.startsWith("--") && x.length > 2))
+    (x.startsWith("-") && x.length === 2 && x !== "--") ||
+    (x.startsWith("--") && x.length > 2)
   );
 }
